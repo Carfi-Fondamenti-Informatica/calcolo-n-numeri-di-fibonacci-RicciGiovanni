5d13f6d0d6383a00e6ede14a55159bf83b1cf037
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-11-11 10:01:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-   return 0;
+int main()
+{
+    int n,i=1,b=1,a=1,c;
+    cin>>n;
+    if(n>=2)
+    {
+        for(i=1;i<=n;i++)
+        {
+         
+         c=a+b;
+         a=b;
+         b=c;
+         cout<<c<<endl;
+        
+        }
+        
+    }
+    else
+    {
+        cout<<"errore"<<endl;
+    }
+
+    return 0;
 }
