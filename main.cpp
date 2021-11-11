e78af7b2b4e5cc39bf1ae7a19a5ce5fea82c5561
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-11-11 10:15:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,22 +6,26 @@ int main()
 {
     int n,i=1,b=1,a=0,c;
     cin>>n;
+    cout<<a<<endl;
+    cout<<b<<endl;
     if(n>=2)
     {
-        for(i=1;i<=n;i++)
+        for(i=2;i<=n;i++)
         {
          
          c=a+b;
+         cout<<c<<endl;
          a=b;
          b=c;
-         cout<<c<<endl;
+         
+         }
         
-        }
+        
         
     }
-    else
+  else
     {
-        cout<<"errore"<<endl;
+       cout<<"errore"<<endl;
     }
 
     return 0;
