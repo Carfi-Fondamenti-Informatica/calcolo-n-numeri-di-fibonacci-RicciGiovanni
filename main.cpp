4fe2173bd82a6be16ac80398e953d8cfe3f35fb4
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-11-14 14:55:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ int main()
 {
     int n,i=1,b=1,a=0,c;
     cin>>n;
-    cout<<a<<endl;
+    
     cout<<b<<endl;
     if(n>=2)
     {
-        for(i=0;i<=n;i++)
+        for(i=2;i<=n;i++)
         {
          
          c=a+b;
